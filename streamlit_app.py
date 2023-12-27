9b73e0e58f11d18e12c6dd836cca3d3ad3073970
duuuhhh <154970483+duuuhhh@users.noreply.github.com>
2023-12-27 15:54:14 +0530

    Update streamlit_app.py
    corrected s3 bucket pointer name

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ streamlit.text('Heat a pressure cooker, add oil in it and pressure cook lentils 
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 
 #To select fruits from the csv list
