2c5f58a8d13af1cb99b5b423b62a348a93ad452c
duuuhhh <154970483+duuuhhh@users.noreply.github.com>
2023-12-27 15:52:18 +0530

    Update streamlit_app.py
    removed the comments
    Addee pickers for the csv fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,15 @@
 import streamlit
-#streamlit.title("Healthy Dinner Options")
-#streamlit.header('Vegetable Khichdi')
-#streamlit.text('Heat a pressure cooker, add oil in it and pressure cook lentils and rice in it.')
+streamlit.title("Healthy Dinner Options")
+streamlit.header('Vegetable Khichdi')
+streamlit.text('Heat a pressure cooker, add oil in it and pressure cook lentils and rice in it.')
 
-#streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+
+
+#To select fruits as checkboxes from the csv list
+streamlit.multiselect ("Pick some fruits:" list(fruit_list.index))
+
+#Display the pverall csv table 
+streamlit.dataframe(fruit_list)
