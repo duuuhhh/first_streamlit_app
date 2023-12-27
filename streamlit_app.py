e12d9e4358144ba5ddbf2678f12f9dabc0029d65
duuuhhh <154970483+duuuhhh@users.noreply.github.com>
2023-12-27 15:47:56 +0530

    Update streamlit_app.py
    Using panda python code

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,7 @@ import streamlit
 #streamlit.header('Vegetable Khichdi')
 #streamlit.text('Heat a pressure cooker, add oil in it and pressure cook lentils and rice in it.')
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+#streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
