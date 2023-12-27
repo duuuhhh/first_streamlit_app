bed585d61ccbaea785c2088cf77b027a990f3d7b
duuuhhh <154970483+duuuhhh@users.noreply.github.com>
2023-12-27 15:56:45 +0530

    Update streamlit_app.py
    Added Fruit Names as index identifier

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ streamlit.text('Heat a pressure cooker, add oil in it and pressure cook lentils 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 import pandas
 fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+fruit_list = fruit_list.set_index('Fruit')
 
 
 #To select fruits from the csv list
