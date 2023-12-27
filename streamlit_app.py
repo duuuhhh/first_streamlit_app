fc0521a3ab4c95d5ebfe94aaca5a848d39df8e58
duuuhhh <154970483+duuuhhh@users.noreply.github.com>
2023-12-27 16:02:04 +0530

    Update streamlit_app.py
    Pre-populating the filter with example fruit names

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,8 @@ fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/d
 #fruit_list = fruit_list.set_index('Serving_Size')
 
 #To select fruits from the csv list
-streamlit.multiselect ("Pick some fruits:",list(fruit_list.Fruit))
+#streamlit.multiselect ("Pick some fruits:",list(fruit_list.Fruit))
+streamlit.multiselect ("Pick some fruits:",list(fruit_list.index),['Avocado','Lime'])
 
 #Display the pverall csv table 
 streamlit.dataframe(fruit_list)
