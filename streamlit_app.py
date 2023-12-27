8730f5f856cb59a8d41abb1081c8c3598b92944e
duuuhhh <154970483+duuuhhh@users.noreply.github.com>
2023-12-27 15:34:14 +0530

    Update streamlit_app.py
    Adding smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
-streamlit.title("Healthy Dinner Options")
-streamlit.header('Vegetable Khichdi')
-streamlit.text('Heat a pressure cooker, add oil in it and pressure cook lentils and rice in it.')
+#streamlit.title("Healthy Dinner Options")
+#streamlit.header('Vegetable Khichdi')
+#streamlit.text('Heat a pressure cooker, add oil in it and pressure cook lentils and rice in it.')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
