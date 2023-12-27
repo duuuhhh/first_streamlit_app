80b28444fd3af26d1963040e80eb836a8a7becd3
duuuhhh <154970483+duuuhhh@users.noreply.github.com>
2023-12-27 15:25:23 +0530

    Update streamlit_app.py
    Added more content to healthy dinner options

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,4 @@
 import streamlit
-streamlit.title("Healthy Dinner")
+streamlit.title("Healthy Dinner Options")
+streamlit.header('Vegetable Khichdi')
+streamlit.text('Heat a pressure cokker, add oil in it and pressure cook lentils and rice in it.')
