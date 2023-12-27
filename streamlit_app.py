201364a7e391eca47218121de2f4eda183194dff
duuuhhh <154970483+duuuhhh@users.noreply.github.com>
2023-12-27 16:21:08 +0530

    Update streamlit_app.py
    Troubleshoot

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,13 +6,13 @@ streamlit.text('Heat a pressure cooker, add oil in it and pressure cook lentils 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 import pandas
 fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(fruit_list)
-#fruit_list = fruit_list.set_index('Fruit')
+
+fruit_list = fruit_list.set_index('Fruit')
 #fruit_list = fruit_list.set_index('Serving_Size')
 
 #To select fruits from the csv list
 #streamlit.multiselect ("Pick some fruits:",list(fruit_list.Fruit))
 streamlit.multiselect ("Pick some fruits:",list(fruit_list.index),['Avocado','Lime'])
 
-#Display the pverall csv table 
-
+#Display the overall csv table 
+streamlit.dataframe(fruit_list)
