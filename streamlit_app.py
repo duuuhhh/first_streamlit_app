1bd5000f4fa91e492f383bd16829142cfe49931a
duuuhhh <154970483+duuuhhh@users.noreply.github.com>
2023-12-29 00:47:31 +0530

    Update streamlit_app.py
    Enhancements

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,3 +30,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 #to display the json in tabular format on the screen
 streamlit.dataframe(fruityvice_normalized)
+
+#Asking user input
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
