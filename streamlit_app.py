dee776d460e4c874a1d4da735b6dbe422df3718f
duuuhhh <154970483+duuuhhh@users.noreply.github.com>
2023-12-29 22:27:15 +0530

    Update streamlit_app.py
    Check something

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,18 +23,25 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 #streamlit.text(fruityvice_response.json())
 
 streamlit.header("Fruityvice Fruit Advice!")
-
+try:
+  fruit_choice=streamlit.text_input("What fruit would you like information about?")
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information")
+  else:
+    fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 # write your own comment -what does the next line do? 
 # To linearize the array formatted json fields (nutrition fields)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 #to display the json in tabular format on the screen
 streamlit.dataframe(fruityvice_normalized)
-
+except URLError as e:
+streamlit.error()
 #Asking user input
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
+streamlit.stop()
 import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
