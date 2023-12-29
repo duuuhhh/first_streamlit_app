4b5df7ab071bea23c5d3e752d9ee55aa57a3fdb3
duuuhhh <154970483+duuuhhh@users.noreply.github.com>
2023-12-29 16:49:32 +0530

    Update streamlit_app.py
    Adding and displaying user entry - jackfruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,9 @@ my_cur = my_cnx.cursor()
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_row = my_cur.fetchone()
-streamlit.text("Fruits Selected:")
+add_my_fruit=streamlit.text_input('What fruit would you like information about?','jackfruit')
+streamlit.write('The user entered ', add_my_fruit)
+streamlit.text("Thanks for adding jackfruit:")
 streamlit.text(my_data_row)
 
 
