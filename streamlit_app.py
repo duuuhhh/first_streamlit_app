9862eaed82bb77c894ce32e9035b29088064c572
duuuhhh <154970483+duuuhhh@users.noreply.github.com>
2023-12-29 22:34:18 +0530

    Update streamlit_app.py
    correction - indentation

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,12 +29,12 @@ try:
     streamlit.error("Please select a fruit to get information")
   else:
     fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 # write your own comment -what does the next line do? 
 # To linearize the array formatted json fields (nutrition fields)
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 #to display the json in tabular format on the screen
-streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
 streamlit.error()
 #Asking user input
